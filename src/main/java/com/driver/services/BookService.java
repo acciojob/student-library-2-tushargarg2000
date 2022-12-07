f479057def1657db3028eea84015f3de67db6c06
Tushar <tushargarg0110@gmail.com>
2022-12-07 19:22:47 +0530

    Changes

--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -1,7 +1,7 @@
 package com.driver.services;
 
-import com.driver.models.Book;
 import com.driver.repositories.BookRepository;
+import com.driver.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +9,6 @@ import java.util.List;
 
 @Service
 public class BookService {
-
 
     @Autowired
     BookRepository bookRepository2;
@@ -19,7 +18,14 @@ public class BookService {
     }
 
     public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> books = null; //find the elements of the list by yourself
-        return books;
+        if(genre != null && author != null){
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        }else if(genre != null){
+            return bookRepository2.findBooksByGenre(genre, available);
+        }else if(author != null){
+            return bookRepository2.findBooksByAuthor(author, available);
+        }else{
+            return bookRepository2.findByAvailability(available);
+        }
     }
 }
